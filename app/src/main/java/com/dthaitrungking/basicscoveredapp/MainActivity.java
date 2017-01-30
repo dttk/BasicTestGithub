acd5b85003e70bf11aa875fb5592f7163ed71fdf
dttk <dthaitrungking@gmail.com>
2017-01-30 14:26:42 +0000

    Basic covered - Drawable

---
+++ app/src/main/java/com/dthaitrungking/basicscoveredapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.dthaitrungking.basicscoveredapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void setHelloText() {
+        TextView helloText = (TextView)findViewById(R.id.textviewid1);
+        helloText.setText("Hello World Through Code");
+
+    }
+}
