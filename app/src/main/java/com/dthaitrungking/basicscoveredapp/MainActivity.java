1c754bb97320a3d4b37ad872e0249d40ab541ce0
dttk <dthaitrungking@gmail.com>
2017-01-30 14:37:47 +0000

    Basic covered - Drawable Change text size call method Change Hello Message

--- app/src/main/java/com/dthaitrungking/basicscoveredapp/MainActivity.java
+++ app/src/main/java/com/dthaitrungking/basicscoveredapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void setHelloText() {
         TextView helloText = (TextView)findViewById(R.id.textviewid1);
-        helloText.setText("Hello World Through Code");
+        helloText.setText("Hello World Again Branch 1");
         helloText.setTextSize(20);
 
     }
